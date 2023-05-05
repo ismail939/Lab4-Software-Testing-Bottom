67f91f57522440ec8af396684cd62d5ab78db0cb
ismail939 <ismailsalama526@gmail.com>
2023-05-05 22:48:12 +0300

    16 tests

--- CoffeeTest/CoffeeMachineTest.java
+++ CoffeeTest/CoffeeMachineTest.java
@@ -391,4 +391,43 @@ class CoffeeMachineTest {
 	    
 	}
 	
+	@Test
+	void startCase5Test16() {    
+		CoffeeMachine cm=new CoffeeMachine();
+		String expected=" ----------------------------------------------------------------\n"
+				+ "|                   Coffee Machine By Manikant                   |\n"
+				+ " ----------------------------------------------------------------\n"
+				+ "\n"
+				+ "Current Status: \n"
+				+ "Available Coffee Power(Gram) 0.0\n"
+				+ "Available Milk(Liter) 0.0\n"
+				+ "Available Water(Liter) 0.0\n"
+				+ "\n"
+				+ " -------------------------------- \n"
+				+ "|1:     Status of Ingredient     |\n"
+				+ " -------------------------------- \n"
+				+ "|2:      Fill Ingredient         |\n"
+				+ " -------------------------------- \n"
+				+ "|3:       Clean Machine          |\n"
+				+ " -------------------------------- \n"
+				+ "|4:        Make Coffee           |\n"
+				+ " -------------------------------- \n"
+				+ "|5: How many Coffee We have made?|\n"
+				+ " -------------------------------- \n"
+				+ "|6:        Exit                  |\n"
+				+ " -------------------------------- \n\n\n"
+				+"\nWe Have Made "+cm.Coffee_Count+" Coffees.\n";
+		
+		
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+	    PrintStream printStream = new PrintStream(baos);
+	    System.setOut(printStream);
+		String userInput = "5";
+		ByteArrayInputStream bais = new ByteArrayInputStream(userInput.getBytes());
+		System.setIn(bais);
+		
+	    cm.start(true);
+	    String actual = baos.toString();
+	    assertEquals(expected, actual, "Output is different than expected");
+	}
 }
